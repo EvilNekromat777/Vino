38f676e81170a082043157cf50b147bb14a54e62
Kireeva <kireeva.en@defo.ru>
2022-02-25 17:32:01 +0300

    с работы

--- js/script.js
+++ js/script.js
@@ -43,44 +43,25 @@ if (isMobile.any()) {
 
 
 // -- меню бургер
-const iconMenu = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.menu__body');
+// const iconMenu = document.querySelector('.menu__icon');
+// const menuBody = document.querySelector('.menu__body');
 
-if(iconMenu){
-    iconMenu.addEventListener('click', function(e){
-        document.body.classList.toggle('_lock');
-        iconMenu.classList.toggle('_active');
-        menuBody.classList.toggle('_active');
-    });
-}
+// if(iconMenu){
+//     iconMenu.addEventListener('click', function(e){
+//         document.body.classList.toggle('_lock');
+//         iconMenu.classList.toggle('_active');
+//         menuBody.classList.toggle('_active');
+//     });
+// }
 
 
-// прокрутка при клике на меню
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-if (menuLinks.length > 0){
-    menuLinks.forEach(menuLink => {
-        menuLink.addEventListener("click", onMenuLinkClick);
-    });
+// Переключение вкладок меню
+$(".menu ul li").click(function(e) {
+    e.preventDefault();
+    $(".menu ul li").removeClass('active');
+    $(this).addClass('active');
+  })
 
-    function onMenuLinkClick(e) {
-        const menuLink = e.target;
-        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
-            const gotoBlock = document.querySelector(menuLink.dataset.goto);
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
 
-            if(iconMenu.classList.contains('_active')){
-                document.body.classList.remove('_lock');
-                iconMenu.classList.remove('_active');
-                menuBody.classList.remove('_active');
-            }
-
-            window.scrollTo({
-                top: gotoBlockValue,
-                behavior: "smooth",
-            });
-            e.preventDefault();
-        }
-    }
-}
 
 

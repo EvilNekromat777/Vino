e8971acf1970fcb5041eca9d6e80f75c7be47d1f
unknown <evil-nekromat@mail.ru>
2022-03-01 21:53:17 +0300

    сделаны характеристики и отзывы

--- js/product-card.js
+++ js/product-card.js
@@ -28,3 +28,50 @@ $(".vintage__item").click(function(e) {
               .addClass("active");
       });
   });
+
+
+  // Описание товара - раскрытие по клику "Читать еще"
+  // Винтаж - раскрытие скрытых табов
+  $(".specifications__description-more").click(function(e) {
+    e.preventDefault();
+    $(".specifications__description").addClass('show');
+    $(".specifications__description-more").addClass('hide');
+  })
+
+
+  // Слайдер "С этим товаром покупают"
+   const swiperTogether = new Swiper('.swiper__together', {
+    loop: true,
+    slidesPerView: 4,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: '.swiper-button-next__together',
+      prevEl: '.swiper-button-prev__together',
+    }
+  });
+
+
+  // Слайдер "Похожие товары"
+  const swiperSimilar = new Swiper('.swiper__similar', {
+    loop: true,
+    slidesPerView: 4,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: '.swiper-button-next__similar',
+      prevEl: '.swiper-button-prev__similar',
+    }
+  });
+
+
+  // Слайдер "Рекомендуемые товары"
+  const swiperRecommended = new Swiper('.swiper__recommended', {
+    loop: true,
+    slidesPerView: 4,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: '.swiper-button-next__recommended',
+      prevEl: '.swiper-button-prev__recommended',
+    }
+  });
+
+  
